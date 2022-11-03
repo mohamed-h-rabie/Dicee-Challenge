7258b5dce88368887c197327187b4c9c69a2e22f
Mohamed Hassan <79491085+mohamed-h-rabie@users.noreply.github.com>
2022-11-03 15:55:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomnumber1 = Math.floor((Math.random()*6)+1)
+var changeofphoto =  "images/dice"+randomnumber1+".png"
+document.querySelector(".img1").setAttribute("src",changeofphoto)
+
+
+var randomnumber2 = Math.floor((Math.random()*6)+1)
+var changeofphoto =  "images/dice"+randomnumber2+".png"
+document.querySelector(".img2").setAttribute("src",changeofphoto)
